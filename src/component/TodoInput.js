51251c2769684004c3be06c99a351fc035b338f7
shhiro <shhiro1991@gmail.com>
2018-07-18 23:16:10 +0900

    todolist complete commit

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -33,6 +33,16 @@ const styles = StyleSheet.create({
 });
 
 export default class TodoInput extends Component {
+    constructor(props) {
+        super(props);
+
+        this.ref = {};
+    } 
+
+    _onPress = () => {
+        this.props.onPress(this.ref._lastNativeText);
+        this.ref.setNativeProps({ text: '' });
+    }
     render() {
         const {
             onPress,
@@ -41,10 +51,13 @@ export default class TodoInput extends Component {
             <View style={styles.container}>
                 <TextInput
                  style={styles.textInput}
-                 onChangeText={text => onPress(text)}
+                 ref={(ref) => { this.ref = ref; }}
                  />
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>追加</Text>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress ={this._onPress}
+                >
+                <Text style={styles.buttonText}>追加</Text>
                 </TouchableOpacity>
             </View>
         );
